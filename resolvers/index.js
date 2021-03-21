58807af077ce29f4353b51c72350d901da1be342
Daniel Fernandes Vieira <danielvieiraufc@gmail.com>
2021-03-21 20:45:32 -0300

    mutations: add create user

--- resolvers/index.js
+++ resolvers/index.js
@@ -1,9 +1,12 @@
 const Query = require('./Query')
+const Mutation = require('./Mutation')
+
 const Produto = require('./Produto')
 const Usuario = require('./Usuario')
 
 module.exports = {
   Query,
+  Mutation,
   Produto,
   Usuario
 }

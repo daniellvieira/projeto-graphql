4382ad9aa1fc05792fb396af8e0d3e3650be87cf
Daniel Fernandes Vieira <danielvieiraufc@gmail.com>
2021-03-21 20:30:17 -0300

    add enum to user

--- data/db.js
+++ data/db.js
@@ -3,19 +3,22 @@ const usuarios = [{
   nome: 'Daniel Vieira',
   email: 'danielfv@gmail.com',
   idade: 29,
-  perfil_id: 1
+  perfil_id: 1,
+  status: 'ATIVO'
 }, {
   id: 2,
   nome: 'Airton Vieira',
   email: 'airton@gmail.com',
   idade: 34,
-  perfil_id: 2
+  perfil_id: 2,
+  status: 'INATIVO'
 }, {
   id: 3,
   nome: 'Iorrana Pinto',
   email: 'iorrana@aaa.com',
   idade: 27,
-  perfil_id: 1
+  perfil_id: 1,
+  status: 'BLOQUEADO'
 }]
 
 const perfis = [
